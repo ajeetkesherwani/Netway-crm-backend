e6cc734b907588129e67c3a7d00f0cffa905906e
anish <anishkr2842003@gmail.com>
2025-10-09 18:26:49 +0530

    report code update

--- src/routes/admin/subscriberReportRoutes.js
+++ src/routes/admin/subscriberReportRoutes.js
@@ -8,8 +8,13 @@ const {
     getAllUsersList
 } = require("../../controllers/admin/reports/subscriber/getAllUsersList");
 
+const {
+    getAllInactiveUsersList
+} = require("../../controllers/admin/reports/subscriber/getAllInactiveUsersList");
+
 const router = express.Router();
 
 router.get("/allUsers", adminAuthenticate, getAllUsersList);
+router.get("/allInactiveUsers", adminAuthenticate, getAllInactiveUsersList);
 
 module.exports = router;
